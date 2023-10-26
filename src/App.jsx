4fcb6745c4a1cffbad7c7b8ee6a09c5dc70a054e
Miracle8221 <anagwumiracle098@gmail.com>
2023-10-26 12:44:06 +0100

    start creating navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+function App() {
+  return (
+    <>
+    <NavBar />
+    <div className='bg-indigo-600 w-full h-screen'></div>
+    </>
+  )
+}
+
+export default App
