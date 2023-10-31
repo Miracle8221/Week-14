59984f9ef44a4ff55b0bdb0acb388938387d80ed
Miracle8221 <anagwumiracle098@gmail.com>
2023-10-31 15:56:29 +0100

    start creating card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import { SideBar } from './components/SideBar'
+import { Body } from './components/body'
 function App() {
   return (
     <>
-    
-    <div className='bg-indigo-600 w-full h-screen'>
+    <div className=' w-full h-screen'>
     <NavBar />
     <SideBar />
-    
+    <div className=' absolute top-[100px] w-90 left-[20%] '>
+    <Body />
+    </div>
     </div>
     </>
+    
+   
+    
     
   )
 }
