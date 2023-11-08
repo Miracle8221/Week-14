0724f54fe749858591e3f1b9009caac6440a2c6a
Miracle8221 <anagwumiracle098@gmail.com>
2023-11-08 11:13:25 +0100

    add picture to the body

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -10,7 +10,7 @@ export const SideBar = () => {
             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                <span className='text-3xl text-indigo-600 mr-1 pt-2'>
                <i className='bx bxl-react'></i>
-            </span>
+               </span>
                <span className="ml-3">React</span>
             </a>
          </li>
