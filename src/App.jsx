b686b7979e12b624b1a614a244410b5d66376c39
Miracle8221 <anagwumiracle098@gmail.com>
2023-11-30 11:53:21 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import { SideBar } from './components/SideBar'
-import {Body}  from './components/Body'
+import Body  from './components/Body'
 import Card from './components/Card'
 import  Footer  from './components/Footer'
 
